e58c8e6b0ca32acc3edc95f42be27e584f401cbe
Negurulxvv <53142647+Negurulxvv@users.noreply.github.com>
2019-08-26 08:02:10 +0300

    Updated AO
    AO

--- shaders/lib/SSAO.glsl
+++ shaders/lib/SSAO.glsl
@@ -1,4 +1,4 @@
-#define AOAmount 1.50	//[1.00 1.25 1.50 1.75 2.00]
+#define AOAmount 0.75	//[0.25 0.50 0.75 1.00 1.25 1.50 1.75 2.00]
 
 vec2 offsetDist(float x, int s){
 	float n = fract(x*1.414)*3.1415;
@@ -18,19 +18,19 @@ float dbao(sampler2D depth, float dither){
 	float sd = 0.0;
 	float angle = 0.0;
 	float dist = 0.0;
-	vec2 scale = 0.6 * vec2(1.0/aspectRatio,1.0) * gbufferProjection[1][1] / (2.74747742 * max(far*d,6.0));
+	vec2 scale = 1.6 * vec2(1.0/aspectRatio,1.0) * gbufferProjection[1][1] / (2.74747742 * max(far*d,6.0));
 
 	for (int i = 1; i <= samples; i++) {
 		vec2 offset = offsetDist(i + dither, samples) * scale;
 
 		sd = ld(texture2D(depth,texcoord.xy+offset).r);
-		float sample = far*(d-sd)*2.0;
+		float sample = far*(d-sd)*3.0;
 		if (hand > 0.5) sample *= 1024.0;
 		angle = clamp(0.5-sample,0.0,1.0);
 		dist = clamp(0.25*sample-1.0,0.0,1.0);
 
 		sd = ld(texture2D(depth,texcoord.xy-offset).r);
-		sample = far*(d-sd)*2.0;
+		sample = far*(d-sd)*3.0;
 		if (hand > 0.5) sample *= 1024.0;
 		angle += clamp(0.5-sample,0.0,1.0);
 		dist += clamp(0.25*sample-1.0,0.0,1.0);
